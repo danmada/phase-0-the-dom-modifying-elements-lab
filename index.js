448b658c61b7c681e3591c4c1561046e23aed034
Daniel Madariaga <danmada@gmail.com>
2021-05-24 17:49:00 -0500

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const noMain = document.getElementById('main');
+noMain.remove();
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Daniel is the champion"
